a8fbf72216a562c52f0ad571b61de47fcef0742f
Meyjan <saragih.meyer0821@gmail.com>
2019-06-20 19:53:56 +0700

    Updates

---
+++ precache-manifest.cad441bc4f7aad7ea68bd8df7ae30571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2421174d2771005fe496a1f386ba0b",
+    "url": "/React_NIM_Finder/index.html"
+  },
+  {
+    "revision": "507f658e98b5d2d4e935",
+    "url": "/React_NIM_Finder/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a80e6ac6670d968745f7",
+    "url": "/React_NIM_Finder/static/css/main.d38b144d.chunk.css"
+  },
+  {
+    "revision": "507f658e98b5d2d4e935",
+    "url": "/React_NIM_Finder/static/js/2.b50d6d06.chunk.js"
+  },
+  {
+    "revision": "a80e6ac6670d968745f7",
+    "url": "/React_NIM_Finder/static/js/main.a0c410d6.chunk.js"
+  },
+  {
+    "revision": "63ac0b229237da3bbab3",
+    "url": "/React_NIM_Finder/static/js/runtime~main.eeb42c4f.js"
+  },
+  {
+    "revision": "5e7cfb5e8aa2ee17fba53b38ccabbf43",
+    "url": "/React_NIM_Finder/static/media/bg.5e7cfb5e.jpg"
+  }
+]);
